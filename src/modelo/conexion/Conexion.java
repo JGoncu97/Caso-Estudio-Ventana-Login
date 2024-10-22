ca65ec7756959e91ee84c23e454dfa532aa55e1e
JGoncu97 <jggc1197@gmail.com>
2024-10-22 09:01:31 -0500

    Properties

--- src/modelo/conexion/Conexion.java
+++ src/modelo/conexion/Conexion.java
@@ -1,21 +1,39 @@
 package modelo.conexion;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
+import java.io.FileInputStream;
 
 import javax.swing.JOptionPane;
 
+
+
 public class Conexion {
-	private String nombreBd="usuario_bd";
+	
+	Properties propiedades = new Properties();
+	/*
+	 * private String nombreBd="usuario_bd";
 	private String usuario="root";
-	private String password="pipe123.";
-	private String url="jdbc:mysql://localhost/"+nombreBd;
+	private String password="1234";
+	private String url="jdbc:mysql://localhost/"+nombreBd;*/
+	
+	private String nombreBd="";
+	private String usuario="";
+	private String password="";
+	private String url="";
 
 	Connection conn=null;
 	//constructor de la clase
 	public Conexion(){
+		cargarCredenciales();
+		
 		try {
+			
 			//obtener el driver
 			Class.forName("com.mysql.jdbc.Driver");
+
 			//obtener la conexion
 			conn=DriverManager.getConnection(url,usuario,password);
 			if (conn!=null) {
@@ -27,6 +45,29 @@ public class Conexion {
 			System.out.println("ocurre una SQLException: "+e.getMessage());
 		}
 	}
+	
+	private void cargarCredenciales() {
+		try {
+			propiedades.load(
+					new FileInputStream("C:\\Users\\SENA\\Desktop\\019\\Caso-Estudio-Ventana-Login\\properties\\credenciales.properties"));
+			
+			nombreBd=propiedades.getProperty("db.nombreBd");
+			usuario=propiedades.getProperty("db.usuario");
+			password=propiedades.getProperty("db.password");
+			url = "jdbc:mysql://localhost:3306/" + nombreBd;
+
+			
+		}catch (FileNotFoundException e) {
+			System.out.println("Error, El archivo no exite");
+			e.printStackTrace();
+		}catch (IOException e) {
+			System.out.println("Error, No se puede leer el archivo");
+			e.printStackTrace();
+			}
+	}
+	
+	
+	
 	public Connection getConnection(){
 		return conn;
 	}
